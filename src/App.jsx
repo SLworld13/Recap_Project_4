36fb84eed1f43524eb5f47a0c77624959f04a6e6
Lars Sperling <155863133+SLworld13@users.noreply.github.com.>
2024-11-25 16:38:46 +0100

    added ColorForm, ColorInput, nanoid for IDs.Inlinestyles, Backgroundcolr direkt mit probs zugewiesen

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>Theme Creator</h1>
+  const [colors, setColors] = useState(initialColors);
 
-      {initialColors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
-    </>
+  const addColor = (newColor) => {
+    setColors([newColor, ...colors]); // Fügt die neue Farbe an den Anfang der Liste
+  };
+
+  return (
+    <div className="App">
+      <h1>Theme Creator</h1>
+      <ColorForm addColor={addColor} />
+      <div className="colors-grid">
+        {colors.map((color) => (
+          <Color key={color.id} color={color} />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -11,7 +11,7 @@ export default function Color({ color }) {
     >
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
-      <p>contrast: {color.contrastText}</p>
+      <p>Contrast: {color.contrastText}</p>
     </div>
   );
 }
